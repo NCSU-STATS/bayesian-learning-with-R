d74117c81e8487775bd76b96d010d76cc5768241
Bruce B Campbell <bruce.bishop.campbell@gmail.com>
2018-04-04 15:17:53 -0400

    040318d

---
+++ install.R
@@ -0,0 +1,11 @@
+install.packages("rjags")
+install.packages("choroplethr") #Mapping
+install.packages("choroplethrMaps")
+install.packages("sf")
+
+install.packages("modeest") # MAP estimate for univariate data 
+
+
+install.packages(c("R2OpenBUGS", "denstrip"))
+
+install.packages("R2jags")
